a45cce440f39c3a25be39cb11014bcb26d617934
shamilkvv <shamiltv803@gmail.com>
2023-12-26 01:10:39 +0400

    Add RBTree class

--- RBTree set/RBTree set.cpp
+++ RBTree set/RBTree set.cpp
@@ -1,7 +1,116 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class RBTree {
+public:
+    class Node {
+    public:
+        bool isRed;
+        int val;
+        Node* parent;
+        Node* left;
+        Node* right;
+
+        Node() : val(0), isRed(true), left(nullptr), right(nullptr) {}
+        Node(int x) : val(x), isRed(true), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+    std::vector <Node*> arr;
+
+    RBTree() : root(nullptr) {}
+    RBTree(int v) : root(new Node(v)) {}
+    ~RBTree() {
+        if (root != nullptr) {
+            destruct(root);
+            delete root;
+        }
+        arr.clear();
+    }
+
+    void destruct(Node* node) {
+        if (node != nullptr) {
+            destruct(node->left);
+            destruct(node->right);
+            delete node;
+        }
+    }
+
+    bool isRed(Node* node) {
+        return (node != nullptr) ? node->isRed : false;
+    }
+
+    void rotateRight(Node*& node) {
+        Node* temp = node->left;
+        node->left = temp->right;
+        temp->right = node;
+
+        temp->isRed = node->isRed;
+        node->isRed = true;
+        node = temp;
+    }
+
+    void rotateLeft(Node*& node) {
+        Node* temp = node->right;
+        node->right = temp->left;
+        temp->left = node;
+        temp->isRed = node->isRed;
+        node->isRed = true;
+        node = temp;
+    }
+
+    void flipColor(Node*& node) {
+        node->isRed = true;
+        if (node->left != nullptr) node->left->isRed = false;
+        if (node->right != nullptr) node->right->isRed = false;
+    }
+
+    void buildTree() {
+        int tempArr[10] = { 3,4,7,1,2,9,8,6,5,10 };
+        for (int i = 0; i != 10; i++) {
+            arr.push_back(new Node(tempArr[i]));
+            insert(root, arr[i]);
+        }
+        display(root);
+    }
+
+    void insert(Node*& node, Node*& vNode) {
+        if (node == nullptr) {
+            node = vNode;
+        }
+        else if (node->val > vNode->val) {
+            insert(node->left, vNode);
+        }
+        else if (node->val < vNode->val) {
+            insert(node->right, vNode);
+        }
+
+        if (isRed(node->right) && !isRed(node->left)) {
+            rotateLeft(node);
+        }
+        if (isRed(node->left) && isRed(node->left->left)) {
+            rotateRight(node);
+        }
+        if (isRed(node->left) && isRed(node->right)) {
+            flipColor(node);
+        }
+    }
+    void display(Node* node) {
+        if (node == NULL) {
+            return;
+        }
+        display(node->left);
+
+        std::cout << node->val << "  ";
+        display(node->right);
+    }
+};
+
+
+int main() {
+
+   
+
+    return 0;
 }
-
